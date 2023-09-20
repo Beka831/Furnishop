fef0563ccd08dcbd1b3081d5fec056b1f0d901f6
Teamir <Teamir124@gmail.com>
2023-09-20 11:55:33 +0300

    edited sidebar menu template

--- resources/views/admin/side.blade.php
+++ resources/views/admin/side.blade.php
@@ -72,9 +72,9 @@
               <span class="menu-title">Products</span>
               <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="ui-basic">
+            <div class="" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="{{ url ('view_product')}}">Add Products</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ url ('/view_product')}}">Add Products</a></li>
                 <li class="nav-item"> <a class="nav-link" href="{{ url ('/show_product')}}">View Products</a></li>
                 
               </ul>
